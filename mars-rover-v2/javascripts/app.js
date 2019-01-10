bc4603836d4dd262662c76ce8b088bc443694a5e
QuevedoIB <45363951+QuevedoIB@users.noreply.github.com>
2019-01-10 22:38:47 +0100

    Add files via upload
    Prework finished

---
+++ mars-rover-v2/javascripts/app.js
@@ -0,0 +1,77 @@
+let rovObj = {
+  direction: "N", 
+}
+
+rovObj.x = 0;
+rovObj.y = 0;
+
+
+
+function turnLeft(direction){
+  console.log("turnLeft was called!");
+  switch (rovObj.direction) {
+    case "N":
+      rovObj.direction === "W"
+    break;
+    case "E":
+      rovObj.direction === "N"
+    break;
+    case "W":
+      rovObj.direction === "S"
+    break;
+    case "S":
+      rovObj.direction === "E"
+    break;
+  }
+}
+
+
+function turnRight(direction){
+  console.log("turnRight was called!");
+  switch (rovObj.direction) {
+    case "N":
+      rovObj.direction === "E"
+    break;
+    case "E":
+      rovObj.direction === "S"
+    break;
+    case "W":
+      rovObj.direction === "N"
+    break;
+    case "S":
+      rovObj.direction === "W"
+    break;
+  }
+}
+
+function moveForward(direction){
+  console.log("moveForward was called")
+  if (rovObj.direction === "N") {
+    rovObj.x -= 1;
+    console.log("Position is: " + rovObj.x + " , " + rovObj.y);
+  } else if (rovObj.direction === "W") {
+    rovObj.y -= 1;
+    console.log("Position is: " + rovObj.x + " , " + rovObj.y);
+  } else if (rovObj.direction === "S") {
+    rovObj.x += 1;
+    console.log("Position is: " + rovObj.x + " , " + rovObj.y);
+  } else if (rovObj.direction === "E") {
+    rovObj.y += 1;
+    console.log("Position is: " + rovObj.x + " , " + rovObj.y);
+  }
+}
+
+function command(string){
+  for (i = 0; i < string.length; i++) {
+    if (string[i] === "f") {
+      moveForward(rovObj.direction);
+    } else if (string[i] === "r") {
+      turnRight(rovObj.direction);
+    } else if (string[i] === "l") {
+      turnLeft(rovObj.direction);
+    }
+  }
+}
+
+console.log(command("rffrfflfrff"));
+
